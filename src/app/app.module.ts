29f3653668e924def6b139477d2b86fc545a2c28
vishwanathathrey <vishwanathathrey@gmail.com>
2022-10-16 14:50:37 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import{HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+
+const appRoute: Routes=[
+  { path:'',component: HomeComponent},
+  { path:'Home',component: HomeComponent},
+  { path:'About',component: AboutComponent},
+  { path:'Contact',component: ContactComponent},
+  { path:'Services',component: ServicesComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
